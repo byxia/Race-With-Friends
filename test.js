70254ae290ea6124bd530b9738efb57a218b4152
Ruoyu Li <lllrrrryyy@gmail.com>
2012-12-14 17:38:19 +0800

    delete unneeded file

--- test.js
+++
@@ -1,7 +0,0 @@
-// testing server for serving the client side google map page
-var express = require("express");
-var app = express();
-var wwwDir = "/static";
-app.use("/", express.static(__dirname + wwwDir));
-app.get("/", function(req, res) { res.render(wwwDir + "/");});
-app.listen(15237);
